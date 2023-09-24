39a86573293609f137a374e050917b2b24cb28e6
Fatah Babakrkhail <fatahbabakrkhail@gmail.com>
2023-09-24 22:42:53 +0430

    nc

--- djangorest/product/views.py
+++ djangorest/product/views.py
@@ -37,8 +37,25 @@ from .serializers import ProductSerializer
 
 
 
+class ProductMixinView(generics.GenericAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+    
 
 
 
