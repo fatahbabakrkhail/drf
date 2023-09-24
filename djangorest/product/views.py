cde2354c11ad50dfcb9ad2e8ba33d1fdfc77a9b7
Fatah Babakrkhail <fatahbabakrkhail@gmail.com>
2023-09-24 16:50:18 +0430

    nc

--- djangorest/product/views.py
+++ djangorest/product/views.py
@@ -2,23 +2,72 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductDetailAPIView(generics.RetrieveAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
 
 
-class ProductListAPIView(generics.ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-class ProductListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-    def perform_create(self, serializer):
-        serializer.validated_data['name'] = float(serializer.validated_data['name'])
-        serializer.validated_data['description'] = float(serializer.validated_data['description'])
-        serializer.validated_data['price'] = float(serializer.validated_data['price'])
 
 
-        serializer.save()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class ProductDetailAPIView(generics.RetrieveAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#
+#
+# class ProductListAPIView(generics.ListAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+# class ProductListCreateAPIView(generics.ListCreateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#
+#     def perform_create(self, serializer):
+#         serializer.validated_data['name'] = float(serializer.validated_data['name'])
+#         serializer.validated_data['description'] = float(serializer.validated_data['description'])
+#         serializer.validated_data['price'] = float(serializer.validated_data['price'])
+#
+#
+#         serializer.save()
+#
